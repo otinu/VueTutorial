1e892b2882eb02b056bfe7fa60267a117a8a9ffb
otinu <fitshter@gmail.com>
2021-11-18 17:18:51 +0900

    [add]loop2/3

---
+++ loop2.js
@@ -0,0 +1,8 @@
+Vue.createApp({
+  data() {
+    return {
+      parentMessage: 'Parent',
+      items: [{ message: 'Foo' }, { message: 'Bar' }]
+    }
+  }
+}).mount('#array-with-index')
---
+++ loop3.js
@@ -0,0 +1,11 @@
+Vue.createApp({
+  data() {
+    return {
+      myObject: {
+        title: 'How to do lists in Vue',
+        author: 'Jane Doe',
+        publishedAt: '2016-04-10'
+      }
+    }
+  }
+}).mount('#v-for-object')
