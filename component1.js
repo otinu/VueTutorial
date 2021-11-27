4475b960251e24dd3fb283e99ed94dfa5a276c40
otinu <fitshter@gmail.com>
2021-11-27 17:24:59 +0900

    [fix]component1

--- component1.js
+++ component1.js
@@ -2,9 +2,9 @@ const TodoList = {
   data() {
     return {
       groceryList: [
-        { id: 0, text: "Vegetables" },
-        { id: 1, text: "Cheese" },
-        { id: 2, text: "Whatever else humans are supposed to eat" },
+        { id: 0, text: "Vegetables", color: "Green" },
+        { id: 1, text: "Cheese", color: "Yellow" },
+        { id: 2, text: "Meat", color: "Red" },
       ],
     };
   },
@@ -15,6 +15,7 @@ const app = Vue.createApp(TodoList);
 app.component("todo-item", {
   props: ["todo"],
   template: `<li>{{ todo.text }}</li>`,
+  template: `<li>{{ todo.color }}</li>` //両方表示することはできない
 });
 
 app.mount("#todo-list-app");
